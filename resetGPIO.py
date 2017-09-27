6d206c5c2c0f028607dc172034196c7ff3849c11
Ivica Vinkovic <ivinkovi@gmail.com>
2017-09-27 21:54:58 +0200

    adding exceptin to startup script

--- resetGPIO.py
+++ resetGPIO.py
@@ -7,14 +7,17 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
 
+try:
+    GPIO.setmode(GPIO.BOARD) 
+    GPIO.setup(31, GPIO.OUT)
+    GPIO.setup(33, GPIO.OUT)
+    GPIO.setup(35, GPIO.OUT)
+    GPIO.setup(37, GPIO.OUT)
+    GPIO.cleanup()
+except:
+    logger.error("E nesto nije u redu")
 
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(31, GPIO.OUT)
-GPIO.setup(33, GPIO.OUT)
-GPIO.setup(35, GPIO.OUT)
-GPIO.setup(37, GPIO.OUT)
 
-GPIO.cleanup()
 
 logger.info('Masina se rebutala i resetirala GPIO')
 
