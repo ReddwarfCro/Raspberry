a3fec321a0fcab66c36fa133689446ed111f7cb7
Ivica Vinkovic <ivinkovi@gmail.com>
2017-09-27 23:31:15 +0200

    Adding reporting of ip address

--- resetGPIO.py
+++ resetGPIO.py
@@ -1,11 +1,32 @@
 import logging
 import RPi.GPIO as GPIO
+import requests
+import socket
+import fcntl
+import struct
+
 logger = logging.getLogger('myapp')
 hdlr = logging.FileHandler('/home/pi/logs/startup.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
+
+def get_ip_address(ifname):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, 
+		struct.pack('256s', ifname[:15])
+	)[20:24])
+
+payload = {'name':str(get_ip_address('wlx3c3300213b66'))}
+
+url = 'http://vinkovicnodejs.herokuapp.com/api/ip'
+#payload = {'name':'test'}
+
+r = requests.post(url, json=payload)
+
+print(r.text)
+print(r.status_code)
 
 try:
     GPIO.setmode(GPIO.BOARD) 
