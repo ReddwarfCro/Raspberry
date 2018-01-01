18286c411b0718617d23f20469751493fb0bf529
Ivica <ivinkovi@gmail.com>
2018-01-01 11:43:04 +0000

    Change network address

--- resetGPIO.py
+++ resetGPIO.py
@@ -10,19 +10,19 @@ logger = logging.getLogger('myapp')
 hdlr = logging.FileHandler('/home/pi/logs/startup.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
+logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 def get_ip_address(ifname):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, 
+	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,
 		struct.pack('256s', ifname[:15])
 	)[20:24])
 
 while True:
     try:
-        logger.info(get_ip_address('wlx3c3300213b66'))
-        payload = {'name':str(get_ip_address('wlx3c3300213b66'))}
+        logger.info(get_ip_address('wlan0'))
+        payload = {'name':str(get_ip_address('wlan0'))}
         url = 'http://vinkovicnodejs.herokuapp.com/api/ip'
         r = requests.post(url, json=payload)
         break
@@ -31,7 +31,7 @@ while True:
 
 while True:
     try:
-        GPIO.setmode(GPIO.BOARD) 
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(31, GPIO.OUT)
         GPIO.setup(33, GPIO.OUT)
         GPIO.setup(35, GPIO.OUT)
