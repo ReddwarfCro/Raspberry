37d758b0bd336b7d7a35457eb6ba99d6f2ca1ad1
Ivica Vinkovic <ivinkovi@gmail.com>
2018-02-04 00:15:41 +0100

    TEst

--- threaded.py
+++ threaded.py
@@ -26,6 +26,17 @@ starttime = dt.now()
 pressed = False
 
 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
 def runWehicle(threadName, where):
     def getch():
         fd = sys.stdin.fileno()
