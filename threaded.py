43a9d76ff1163b4d9d01e8ba9c5b8283430d073b
Ivica Vinkovic <ivinkovi@gmail.com>
2018-02-04 01:10:19 +0100

    TEst

--- threaded.py
+++ threaded.py
@@ -1,112 +1,30 @@
-import RPi.GPIO as GPIO
-from datetime import datetime as dt
-import sys, tty, termios, time, threading
+import threading
+import time
 
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      print_time(self.name, self.counter, 5)
+      print ("Exiting " + self.name)
 
-class myTh(threading.Thread):
-    def __init__(self, threadID, name, where):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.where = where
+def print_time(threadName, delay, counter):
+   while counter:
+      time.sleep(delay)
+      print ("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1
 
-    def run(self):
-        runWehicle(self.name, self.where)
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
 
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(31, GPIO.OUT)
-GPIO.setup(33, GPIO.OUT)
-GPIO.setup(35, GPIO.OUT)
-GPIO.setup(37, GPIO.OUT)
-
-initial = True
-state = False
-starttime = dt.now()
-pressed = False
-
-
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-
-def runWehicle(threadName, where):
-    def getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    def LightLED(LedId, state):
-        GPIO.output(LedId, state)
-
-    run = True
-    ct = dt.now()
-    while run:
-        char = getch()
-        LightLED(33, 0)
-        LightLED(35, 0)
-        if (char != "w" or ((dt.now() - ct).total_seconds() > 0.1)):
-            run = False
-            break
-        time.sleep(0.01)
-        LightLED(33, 1)
-        LightLED(35, 1)
-        time.sleep(0.01)
-
-
-def TurnOff():
-    GPIO.output(31, False)
-    GPIO.output(33, False)
-    GPIO.output(35, False)
-    GPIO.output(37, False)
-
-
-TurnOff()
-
-print("Spreman!")
-
-while True:
-    char = getch()
-    if (char == "w"):
-        # print("naprijed")
-        thf = myTh(1, "Thread-1", "f")
-        thf.start()
-
-    if (char == "s"):
-        # print("nazad")
-        thb = myTh(2, "Thread-2", "b")
-        thb.start()
-
-    if (char == "a"):
-        # print("desno")
-        thr = myTh(3, "Thread-3", "r")
-        thr.start()
-
-    if (char == "d"):
-        # print("lijevo")
-        thl = myTh(4, "Thread-4", "l")
-        thl.start()
-
-    if (char == "x"):
-        print("Kraj")
-        break
-
-try:
-    # RunForward(0.0001)
-    # RunBackward(0.0001)
-    GPIO.cleanup()
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+# Start new Threads
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print ("Exiting Main Thread")
