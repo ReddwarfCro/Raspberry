7fb23b610ec3964236c3018f0f78a474aecfa075
Ivica Vinkovic <ivinkovi@gmail.com>
2018-03-18 10:20:15 +0100

    TEst

---
+++ RunMotor.py
@@ -0,0 +1,73 @@
+import RPi.GPIO as GPIO
+from datetime import datetime as dt
+import sys, tty, termios, time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(31, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)
+GPIO.setup(35, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+
+initial = True
+state = False
+starttime = dt.now()
+pressed = False
+
+forwardLeft = GPIO.PWM(31, 0.5)
+reverseLeft = GPIO.PWM(33, 0.5)
+
+forwardRight = GPIO.PWM(37, 0.5)
+reverseRight = GPIO.PWM(35, 0.5)
+
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def TurnOff():
+    forwardLeft.stop()
+    reverseLeft.stop()
+    forwardRight.stop()
+    reverseRight.stop()
+
+TurnOff()
+
+print("Spreman!")
+
+while True:
+    char = getch()
+    if(char == "w"):
+        forwardLeft.ChangeDutyCycle(50)
+        forwardRight.ChangeDutyCycle(50)
+
+    #if(char == "s"):
+        #print("nazad")
+        #RunBackward(0.01)
+
+    #if(char == "d"):
+        #print("desno")
+        #RunRight(0.01)
+
+    #if(char == "a"):
+        #print("lijevo")
+        #RunLeft(0.01)
+
+    if(char == "x"):
+        TurnOff()
+        break
+
+
+try:
+    #RunForward(0.0001)
+    #RunBackward(0.0001)
+    GPIO.cleanup()
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
