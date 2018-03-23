53ca2f3f4a62423f0c3a530a621ce382f008292f
Ivica Vinkovic <ivinkovi@gmail.com>
2018-03-23 01:07:57 +0100

    TEst

--- test.py
+++ test.py
@@ -1,34 +1,58 @@
+# Libraries
+import RPi.GPIO as GPIO
 import time
 
-import pigpio
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BOARD)
 
-TRIGGER=18
-ECHO=24
+# set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
 
-high_tick = None # global to hold high tick.
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def cbfunc(gpio, level, tick):
-   global high_tick
-   if level == 0: # echo line changed from high to low.
-      if high_tick is not None:
-         echo = pigpio.tickDiff(high_tick, tick)
-         cms = (echo / 1000000.0) * 34030 / 2
-         print("echo was {} micros long ({:.1f} cms)".format(echo, cms))
-   else:
-      high_tick = tick
 
-pi = pigpio.pi() # Connect to local Pi.
+def distance():
+    time.sleep(0.01)
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
 
-pi.set_mode(TRIGGER, pigpio.OUTPUT)
-pi.set_mode(ECHO, pigpio.INPUT)
 
-cb = pi.callback(ECHO, pigpio.EITHER_EDGE, cbfunc)
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
 
-start = time.time()
+    StartTime = time.time()
+    StopTime = time.time()
 
-while (time.time()-start) < 60:
-   pi.gpio_trigger(TRIGGER, 10)
-   time.sleep(0.1)
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
 
-cb.cancel() # Cancel callback.
-pi.stop() # Close connection to Pi
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            if dist < 40:
+                print("Measured Distance = %.1f cm" % dist)
+            time.sleep(0.1)
+
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
