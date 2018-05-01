6bda140ae0cc17384b23c5184182b4f4771addaf
Ivica Vinkovic <ivinkovi@gmail.com>
2018-05-01 21:28:32 +0200

    TEst

---
+++ proximity.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+pin = 40
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(pin, GPIO.IN)
+
+input = GPIO.input(pin)
+
+while True:
+	if (!GPIO.input(pin)):
+		print("In Range")
+	time.sleep(1)
