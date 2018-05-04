fad935ba059a5ac3d1c28066eaeb1d5bda8d9ea6
Ivica Vinkovic <ivinkovi@gmail.com>
2018-05-04 22:53:23 +0200

    TEst

--- Servo.py
+++ Servo.py
@@ -8,29 +8,25 @@ GPIO.setup(03, GPIO.OUT)
 pwm=GPIO.PWM(03, 50)
 
 pwm.start(7.5)
-
+wait = 0.5
 try:
 	while True:
 		pwm.ChangeDutyCycle(7.5)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(9.1)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(10.7)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(12.5)
-		sleep(1)
-		pwm.ChangeDutyCycle(10.7)
-		sleep(1)
-		pwm.ChangeDutyCycle(9.1)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(7.5)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(5.9)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(4.3)
-		sleep(1)
+		sleep(wait)
 		pwm.ChangeDutyCycle(2.5)
-		sleep(1)
+		sleep(wait)
 
 except KeyboardInterrupt:
 	pwm.stop()
