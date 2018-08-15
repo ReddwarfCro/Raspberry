b0486a2c646535435212453c262373c70e38d70b
Ivica Vinkovic <ivinkovi@gmail.com>
2018-08-15 14:58:57 +0200

    TEst

--- Servo.py
+++ Servo.py
@@ -1,31 +1,75 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(03, GPIO.OUT)
 
+# set GPIO Pins
+GPIO_TRIGGER = 12
+GPIO_ECHO = 18
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
 pwm=GPIO.PWM(03, 50)
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 pwm.start(7.5)
 wait = 0.5
+
 try:
 	while True:
 		pwm.ChangeDutyCycle(7.5)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(9.1)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(10.7)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(12.5)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(7.5)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(5.9)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(4.3)
+        print(distance())
 		sleep(wait)
 		pwm.ChangeDutyCycle(2.5)
+        print(distance())
 		sleep(wait)
 
 except KeyboardInterrupt:
